f88261517ede89b47a4e36300f8e715c1ff7b5e8
youbot <youbot>
2017-06-08 16:57:15 +0200

    node file modified

--- packages/g3_bnt/ros/scripts/bnt.py
+++ packages/g3_bnt/ros/scripts/bnt.py
@@ -3,9 +3,11 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import String
 import tf
+from math import radians, degrees
+
 
 def sender():
-    pub = rospy.Publisher('goal', PoseStamped, queue_size=10)
+    pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=10)
     rospy.init_node('input_test')
     file_name = rospy.get_param("bnt/goals_file")
     file = open(file_name,"r")
@@ -19,7 +21,7 @@ def sender():
     	value = [float(s) for s in temp[1:4]]
     	database[key] = value
     
-    print database	
+    print "list of workspaces \n " +str(database)	
 
     # rate = rospy.Rate(10)
     pos = PoseStamped()
@@ -31,12 +33,12 @@ def sender():
 		
 	    value = database[loc]
 	    # Euler to quaternion
-	    q = tf.transformations.quaternion_from_euler(value[0], value[1], value[2])
+	    q = tf.transformations.quaternion_from_euler(0, 0, value[2])
 	    pos.pose.position.x = value[0]
 	    pos.pose.position.y = value[1]
 	    pos.pose.orientation.z = q[2]
 	    pos.pose.orientation.w = q[3]
-	
+	    pos.header.frame_id = 'map'	
 	    rospy.loginfo('goal sent')
 	    rospy.loginfo('goal is being executed')
 	    pub.publish(pos)
